89356222af716b4f8c62228e27041784b7cbcbb9
Autumn Loveless <24770560+autumnloveless@users.noreply.github.com>
2021-07-07 12:22:10 -0400

    Limit account search to user accounts

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -8,18 +8,16 @@ exports.find = async (req, res) => {
 }
 
 exports.list = async (req, res) => {
-  result = await Account.list()
+  result = await Account.list({userId: req.user.id})
   res.status(result.success ? 200 : 400).json(result)
 }
 
 exports.delete = async (req, res) => {
-  if(req.user.id != req.params.userId) { return res.status(403).json({ error: 'unauthorized' }) }
   result = await Account.delete(req.params.userId)
   res.status(result.success ? 200 : 400).json(result)
 }
 
 exports.update = async (req, res) => {
-  if(req.user.id != req.params.userId) { return res.status(403).json({ error: 'unauthorized' }) }
   result = await Account.update(req.params.userId, req.body)
   if(result.user) { delete result.user.dataValues.password }
   res.status(result.success ? 200 : 400).json(result)
